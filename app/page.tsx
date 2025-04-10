755c43baabf777b0ef902d904b1ea3f144066731
Jakub Mierzejek <gemixe1234@gmail.com>
2025-04-10 18:28:41 +0000

    installed tailwind and motion

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,10 @@ import fetchFood from "./api/fetchFood"
 import {useEffect, useState} from "react"
 
 const Home = () => {
-  const [foodData, setFoodData] = useState<any>()
-  useEffect(() => {
-    const getFood = async () => {
-      const data = await fetchFood()
-      setFoodData(data)
-    };
-    getFood()
-  }, []);
 
-  console.log(foodData)
   return (
-    <div>
-    {Array.isArray(foodData) && foodData.length > 0 ? (
-      foodData.map((recipe, index) => (
-        <div key={index}>
-          <img src={recipe.image}></img>
-          <p>{recipe.title}</p>
-        </div>
-      ))
-    ) : (
-      <p>loading...</p>
-    )}
+    <div className="text-3xl font-bold underline">
+      hello
     </div>
     
   );
